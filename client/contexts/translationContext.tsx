b3769fdd2543d1e9cdb0383d596f92b97056f31c
Carolina Valladares <carolinalimacvalladares@gmail.com>
2023-09-27 11:55:00 -0300

    fix: localstorage error in translation context

--- client/contexts/translationContext.tsx
+++ client/contexts/translationContext.tsx
@@ -16,18 +16,27 @@ interface IProps {
 }
 
 export default function TranslationContextProvider({ children }: IProps) {
-  const [language, setLanguage] = useState<keyof ITranslations>(
-    localStorage.getItem("notably_language")
-      ? (localStorage.getItem("notably_language") as keyof ITranslations)
-      : // browser language
-      navigator.language == "pt-BR" || "pt-PT"
-      ? "portuguese"
-      : "english"
-  );
+  const [language, setLanguage] = useState<keyof ITranslations>("portuguese");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (window.localStorage.getItem("notably_language")) {
+        setLanguage(
+          window.localStorage.getItem("notably_language") as keyof ITranslations
+        );
+      } else if (window.navigator.language == "pt-BR" || "pt-PT") {
+        setLanguage("portuguese");
+      } else {
+        setLanguage("english");
+      }
+    }
+  }, []);
 
   // when language changes set localStorage to current language
   useEffect(() => {
-    localStorage.setItem("notably_language", language);
+    if (typeof window !== "undefined") {
+      window.localStorage.setItem("notably_language", language);
+    }
   }, [language]);
 
   return (
