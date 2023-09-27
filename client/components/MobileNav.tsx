56915ebbeb65e38b1492efbbba2c62331af011c4
Carolina Valladares <carolinalimacvalladares@gmail.com>
2023-09-26 23:50:50 -0300

    add logout btn to mobile nav

--- client/components/MobileNav.tsx
+++ client/components/MobileNav.tsx
@@ -3,10 +3,21 @@ import useTranslation from "@/hooks/useTranslation";
 import Avatar from "./Avatar";
 import formatHandle from "@/utils/formatHandle";
 import TRANSLATIONS from "@/CONSTS/translations";
+import { toast } from "react-toastify";
+import { LogOut } from "lucide-react";
 
 const MobileNav = () => {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
   const { language } = useTranslation();
+
+  const handleLogout = async () => {
+    try {
+      await signOut();
+    } catch (e) {
+      return toast.error(TRANSLATIONS[language].validation.logoutFailed);
+    }
+  };
+
   return (
     <div className="fixed bottom-0 left-0 w-full bg-background-primary text-text-color shadow-md shadow-shadow-color py-2 px-4 ">
       <div className="max-w-lg m-auto flex items-center justify-between">
@@ -27,7 +38,16 @@ const MobileNav = () => {
           </div>
         )}
 
-        <div></div>
+        <div>
+          {/* Logout button */}
+          <button
+            onClick={handleLogout}
+            title={TRANSLATIONS[language].labels.logout}
+            className="flex items-center justify-center gap-1 text-xs text-rose-500 m-auto font-medium"
+          >
+            <LogOut size={20} strokeWidth={2} />
+          </button>
+        </div>
       </div>
     </div>
   );
